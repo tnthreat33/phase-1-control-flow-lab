28b8932316f00286d0d39e578e9b1261201f05ba
Rachel Hamby <tnthreat33@gmail.com>
2023-09-28 19:57:17 -0400

    commit tree a0ad9758e1f9a2ef1f86157a28ac8ffe98f63e52

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
   // Write your code here!
+  if (num <= 400){
+    return 'This one is on me!'
+  } else if (num > 400 && num <= 2000){
+    return 'That will be twenty bucks.'
+  }else if(num > 2000 && num <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  }else{
+   return  'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city !== "NYC" ? 'No go.' : 'Ok, sounds good.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if(tip === "generous"){
+    return "Thank you so much."
+  } else if (tip === "not as generous"){
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
 }
